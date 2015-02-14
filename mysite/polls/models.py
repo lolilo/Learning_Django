70a0bf4af8c1e05890f9268af969a09a12eba4d4
Liana Lo <liana.lo@prezi.com>
2015-02-14 23:25:41 +0100

    include polls app, migrate DB

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,6 +5,6 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Question)
-    choice = models.CharField(max_length=200)
-    votes = model.IntegerField(default=0)
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
